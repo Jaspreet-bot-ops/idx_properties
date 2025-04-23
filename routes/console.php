dfea9d221f6557ca062add8edae32d74699e7ec7
jp <jp@mangoitsolutions.com>
2025-04-23 15:07:38 +0530

    added

--- routes/console.php
+++ routes/console.php
@@ -2,19 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Foundation\Configuration\Schedule;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
 
+Schedule::call(function () {
+    Log::info('✅ Scheduled task executed at: ' . now());
+})->everyMinute();
+
+Schedule::command('update:trestle-properties --geocode --hours=2')
+->everyMinute()
+->withoutOverlapping()
+->appendOutputTo(storage_path('logs/laravel.log'));
+
+
+// Define custom Artisan command
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-return function (Schedule $schedule) {
-    // $schedule->command('update:trestle-properties --geocode --hours=2')
-    //         ->everyTwoHours()
-    //         ->withoutOverlapping()
-    //         ->appendOutputTo(storage_path('logs/trestle-update.log'));
-    $schedule->command('update:trestle-properties --geocode --hours=2')
-        ->everyMinute() // Run every minute during testing
-        ->withoutOverlapping()
-        ->appendOutputTo(storage_path('logs/trestle-update.log'));
-};
