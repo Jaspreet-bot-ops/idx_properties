961ad245b326d880e70990c4b666238eb24d98d2
jp <jp@mangoitsolutions.com>
2025-04-28 17:49:25 +0530

    back

--- app/Http/Controllers/Api/PropertyApiController.php
+++ app/Http/Controllers/Api/PropertyApiController.php
@@ -233,8 +233,6 @@ class PropertyApiController extends Controller
                 'StreetNumber',
                 'StreetName',
                 'City',
-                'StreetDirPrefix',
-                'StreetSuffix',
                 'StateOrProvince',
                 'PostalCode',
                 'PropertySubType',
@@ -335,6 +333,10 @@ class PropertyApiController extends Controller
                 'id' => $building->representative_id,
                 'building_name' => $buildingName,
                 'address' => $buildingName,
+                'full_address' => $building->StreetNumber . ' ' .  $building->StreetName .
+                    ($city ? ', ' . $city : '') .
+                    ($building->StateOrProvince ? ', ' . $building->StateOrProvince : '') .
+                    ($building->PostalCode ? ' ' . $building->PostalCode : ''),
                 'city' => $city,
                 'state' => $building->StateOrProvince,
                 'postal_code' => $building->PostalCode,
